d61ba81e59c380b2c6de4cb6acbd2c04a5e6bd05
Marwa-AlHajriy <160975950+Marwa-AlHajriy@users.noreply.github.com>
2024-11-13 23:08:43 +0000

    Create Models.py
    Function to test different regression models

---
+++ Models.py
@@ -0,0 +1,63 @@
+
+
+
+def find_best_model_using_gridsearchcv(X, y):
+    algorithms = {
+        'linear_regression': {
+            'model': LinearRegression(),
+            'params': {
+                'fit_intercept': [True, False]
+            }
+        },
+        'lasso': {
+            'model': Lasso(),
+            'params': {
+                'alpha': [0.1, 1],  # Adjusted alpha values
+                'selection': ['random', 'cyclic']
+            }
+        },
+        'decision_tree': {
+            'model': DecisionTreeRegressor(n_jobs=-1, random_state=123),
+            'params': {
+                'criterion': ['squared_error', 'friedman_mse'],
+                'splitter': ['best', 'random']
+            }
+        },
+        'random_forest': {
+            'model': RandomForestRegressor(n_jobs=-1, random_state=123),
+            'params': {
+                'n_estimators': [50,100]
+            }
+                }
+        ,
+
+        'xgboost': {
+            'model': xgb.XGBRegressor(n_jobs=-1, random_state=123),
+            'params':{
+                'n_estimators': [100, 200], 
+                'max_depth': [3, 5]
+            }
+        }
+    }
+    
+    scores = []
+    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
+
+    for algo_name, config in algorithms.items():
+        try:
+            gs = GridSearchCV(config['model'], config['params'], cv=cv,
+                              return_train_score=False, n_jobs=-1)
+            gs.fit(X, y)
+            scores.append({
+                'model': algo_name,
+                'best_score_': gs.best_score_,
+                'best_params_': gs.best_params_
+            })
+        except Exception as e:
+            print(f"Error in {algo_name}: {e}")
+
+    return pd.DataFrame(scores)
+
+# Call the function
+results = find_best_model_using_gridsearchcv(x_nonvintage, y_nonvintage)
+results
